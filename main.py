38807b7f88cd047443192e230c82095cec27ba77
AlexGavrilova <sasha-17051994@yandex.ru>
2015-06-22 21:51:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,92 @@
+import ClassLN
 import sys
-import longArifmetics
-import functions
+import os
 
-functions.PrintStart()
+if len(sys.argv) < 5 or len(sys.argv) > 7:
+        print "You must be enter more than 5 and fewer than 7 arguments. For example:"
+	print "<file_a> <[ + | - | x | / | % | ^ ]> <file_b> <file_result> <file_module>"
+	print "or:"
+	print "<file_a> <[ + | - | x | / | % | ^ ]> <file_b> <file_result> <file_module> <-b>"
+	sys.exit(0)
 
-paramsNum = len(sys.argv)
+bin = 0
 
-if not functions.checkParam(paramsNum, sys.argv):
-	sys.exit(-1)
+if len(sys.argv) == 5:
+    if sys.argv[2][0] == '^' :
+            print "You must be enter module file"
+            sys.exit(0)
 
-inFile1 = sys.argv[1]
-operation = sys.argv[2][0]
-inFile2 = sys.argv[3]
-resFile = sys.argv[4]
-base = 0
-isModule = False
+if len(sys.argv) == 6:
+    if sys.argv[5] == "-b":
+        bin = 1
+    else:
+        bin = 0
+    if bin == 0:
+        if sys.argv[2][0] != '^':
+            print "Bad argument: ", sys.argv[5]
+            sys.exit(0)
+        else:
+            if not os.path.isfile(sys.argv[5]):
+                print "Module file not found: ", sys.argv[5]
+	        sys.exit(0)
 
-if (paramsNum == 5):
-	base = 1000000000
+if len(sys.argv) == 7:
+    if sys.argv[6] == "-b":
+        bin = 1;
+    else:
+        print "What this: ", sys.argv[6]
+        sys.exit(0)
+    if sys.argv[2][0] != '^':
+        print "Bad argument: ", sys.argv[5]
+        sys.exit(0)
+    else:
+        if not os.path.isfile(sys.argv[5]):
+            print "Module file not found: ", sys.argv[5]
+	    sys.exit(0)
 
-if (paramsNum == 6):
-	if (sys.argv[5] == "-b"):
-		base = 256
-		binKey = sys.argv[5]
-	else:
-		base = 1000000000
-		moduleFile = sys.argv[5]
-		isModule = True
+if not os.path.exists(sys.argv[1]):
+    print "File not found: ", sys.argv[1]
+    sys.exit(0)
 
-if (paramsNum == 7):
-	if (sys.argv[5] == "-b"):
-			base = 256
-			binKey = sys.argv[5]
-			moduleFile = sys.argv[6]
-			isModule = True
-	else:
-		print "Too many arguments. Error!"
-		sys.exit(-1)
-		
-firstNum = longArifmetics.longNum()
-secNum = longArifmetics.longNum()
-resultNum = longArifmetics.longNum()
+if not os.path.isfile(sys.argv[3]):
+    print "File not found: ", sys.argv[3]
+    sys.exit(0)
 
-if not firstNum.read(inFile1, base):
-		print "An error occured in reading the first number!"
-		sys.exit(-1)
-if not secNum.read(inFile2, base):
-		print "An error occured in reading the second number!"
-		sys.exit(-1)
-		
-if operation == "+": 
-	resultNum = firstNum + secNum
-elif operation == "-":
-	resultNum = firstNum - secNum
-elif operation == "m":
-	resultNum = firstNum * secNum
-elif operation == "/":
-	resultNum = firstNum / secNum
-elif operation == "^":
-	resultNum = firstNum ^ secNum
-elif operation == "r":
-	resultNum = firstNum % secNum
-	
-if isModule:
-	modlongNum = longArifmetics.longNum()
-	resModlongNum = longArifmetics.longNum()
-	if not modlongNum.Read(moduleFile, base):
-		print "An error occured in reading the module!"
-		sys.exit(-1)
-		
-	resModlongNum = resultNum % modlongNum;
-	resModlongNum.write(resFile)
+if ((len(sys.argv[2]) > 1 or sys.argv[2][0] == '\0') or sys.argv[2][0] != '+' and sys.argv[2][0] != '-' and sys.argv[2][0] != 'x' and sys.argv[2][0] != '/' and sys.argv[2][0] != '%' and sys.argv[2][0] != '^'):
+    print "Bad operation: ", sys.argv[2][0]
+    sys.exit(0) 
+
+a = ClassLN.ClassLN()
+b = ClassLN.ClassLN()
+
+if bin == 1:
+    a.ReadBin(sys.argv[1])
+    b.ReadBin(sys.argv[3])
 else:
-	resultNum.write(resFile)
+    a.ReadText(sys.argv[1])
+    b.ReadText(sys.argv[3])    
+
+result = ClassLN.ClassLN()
+
+if sys.argv[2][0] == '+':
+    result = a + b
+if sys.argv[2][0] == '-':
+    result = a - b
+if sys.argv[2][0] == 'x':
+    result = a * b
+if sys.argv[2][0] == '/':
+    result = a / b
+if sys.argv[2][0] == '%':
+    result = a % b    
+if sys.argv[2][0] == '^':
+    c = ClassLN.ClassLN()
+    if bin == 1:
+	c.ReadBin(sys.argv[5])
+    else:
+	c.ReadText(sys.argv[5])
+    result = ClassLN.PowMod(a, b, c)
+
+if bin == 1:
+    result.WriteBin(sys.argv[4])
+else:
+    result.WriteText(sys.argv[4])
